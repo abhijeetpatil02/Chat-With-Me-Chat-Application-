9fc0dc15f3aa35f8c5fb225d1a242f05f1cddbac
abhijeetpatil02 <patilabhijeet261@gmail.com>
2026-09-07 17:35:02 +0530

    update feature

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const mysql2 = require('mysql2');
 const webpush = require('web-push');
 const app = express();
 
-// your PWAA / service worker code
+// your PWA / servicee worker code
 // app.use(express.static("public"));
 
 app.use(express.json());
